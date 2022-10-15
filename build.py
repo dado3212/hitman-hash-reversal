a950ca44b20bc7518e8c4212cfc78f887411f424
Alex Beals <alexcbeals@gmail.com>
2022-10-15 09:56:32 -0700

    Update build.py to use the full list of known strings, even for non-Hitman 3 games

--- build.py
+++ build.py
@@ -1,25 +1,39 @@
-import json, hashlib, re, pickle
-from typing import Optional, List
+import json, re, pickle, zipfile, os, requests
+from typing import List, Dict
 
-def hash_to_hex(hash: int) -> str:
-    return format(hash, 'x').upper().rjust(16, '0')
+print('Starting!')
+if not os.path.exists('hashes.json'):
+    print("hashes.json doesn't exit. Please extract it from my hitman-hashes-json repo.")
+    exit()
 
-def hex_to_hash(hex: str) -> int:
-    return int(hex.lstrip('0'), 16)
+print('Downloading hash list...')
+try:
+    os.remove('./hash_list.txt')
+except OSError:
+    pass
 
-def ioi_string_to_hex(path: str) -> str:
-    raw = hashlib.md5(path.encode()).hexdigest().upper()
-    return '00' + raw[2:16]
+r = requests.get('https://hitmandb.notex.app/latest-hashes.zip', stream=True)
+with open('hash_list.zip', 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=128):
+        fd.write(chunk)
 
-print('Starting')
+with zipfile.ZipFile('hash_list.zip') as zf:
+    zf.extractall('./')
+
+os.remove('hash_list.zip')
+print('Extracted hash_list.txt.')
+
+print('Building hashes pickle...')
 with open('hashes.json', 'r') as f:
     data = json.load(f)
-    print('Loaded')
+    print('Loaded hashes.json.')
 
 with open('./hashes.pickle', 'wb') as handle:
     pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+print('Done.')
 
-reverse = dict()
+print('Building reverse pickle...')
+reverse: Dict[str, List[str]] = dict()
 for hash in data:
     for depends in data[hash]['depends']:
         depends = str(depends)
@@ -29,23 +43,42 @@ for hash in data:
 
 with open('./reverse.pickle', 'wb') as handle:
     pickle.dump(reverse, handle, protocol=pickle.HIGHEST_PROTOCOL)
+print('Done.')
 
-# Build text patterns
+print('Building pattern pickes...')
+# Build patterns
 texture_folders = set([])
 texture_suffixes = set([])
-for hash in data:
-    if data[hash]['type'] == 'TEXT':
-        name = data[hash]['name']
-        if len(name) > 0 and 'texture' in name:
-            info = re.search(r"^(.*)/([^\\]*)\.texture(.*)$", name, re.IGNORECASE)
-            if info is None:
-                print(name)
-                break
-            else:
-                texture_folders.add(info.group(1))
-                texture_suffixes.add(info.group(3))
+material_folders = set([])
+with open('hash_list.txt', 'r') as f:
+    # completion
+    f.readline()
+    # hashes count
+    f.readline()
+    # version number
+    f.readline()
 
-print(texture_folders)
+    for line in f.readlines():
+        split = line.split(',', 1)
+        ioi_string = split[1].rstrip()
+        extension = split[0][-4:]
+        if extension == 'TEXT':
+            if 'texture' in ioi_string:
+                info = re.search(r"^(.*)/([^\\]*)\.texture(.*)$", ioi_string, re.IGNORECASE)
+                if info is None:
+                    print(ioi_string)
+                    break
+                else:
+                    texture_folders.add(info.group(1))
+                    texture_suffixes.add(info.group(3))
+        elif extension == 'MATI':
+            if 'assembly' in ioi_string:
+                info = re.search(r"^(.*)/[^\\]*\.mi\]\.pc_mi$", ioi_string, re.IGNORECASE)
+                if info is None:
+                    print(ioi_string)
+                    break
+                else:
+                    material_folders.add(info.group(1))
 
 with open('./texture_folders.pickle', 'wb') as handle:
     pickle.dump(texture_folders, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -53,20 +86,7 @@ with open('./texture_folders.pickle', 'wb') as handle:
 with open('./texture_suffixes.pickle', 'wb') as handle:
     pickle.dump(texture_suffixes, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-# Build material patterns
-material_folders = set([])
-for hash in data:
-    if data[hash]['type'] == 'MATI':
-        name = data[hash]['name']
-        if 'assembly' in name:
-            info = re.search(r"^(.*)/[^\\]*\.mi\]\.pc_mi$", name, re.IGNORECASE)
-            if info is None:
-                print(name)
-                break
-            else:
-                material_folders.add(info.group(1))
-
-print(material_folders)
-
 with open('./material_folders.pickle', 'wb') as handle:
     pickle.dump(material_folders, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+print('Done. Closing!')
